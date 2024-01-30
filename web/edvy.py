ab47b1428b97778c4637db030d60f6f5684e9750
Motunrayo <ajirotutu4real@gmail.com>
2024-01-30 10:36:07 +0100

    Added some setup

---
+++ web/edvy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Route for Web"""
+from flask import Flask, render_template, make_response, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def home():
+    """Retrieve home """
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000, debug="True")
