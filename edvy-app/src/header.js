00cb88b00dd86f28534ba94638932ddadef0eef0
Motunrayo <ajirotutu4real@gmail.com>
2024-01-30 16:53:22 +0100

    Added react setup

---
+++ edvy-app/src/header.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import './header.css';
+
+export default function Header() {
+  return (
+      <header>
+        <div className="logo">
+        <img src="" className="App-logo" alt="logo" />
+        </div>
+        <div className="header-nav">
+            <h4>Login</h4>
+            <h4>Sign-up</h4>
+        </div>
+      </header>
+  );
+}
