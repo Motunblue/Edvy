79ba79191d16177d0386f99907ce0413042304ab
Motunrayo <ajirotutu4real@gmail.com>
2024-01-30 18:12:01 +0100

    Added header section for home

---
+++ edvy-app/src/home.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+import './home.css';
+
+export default function Home() {
+  return (
+      <section className="intro">
+        <div className="intro-word">
+            <p className="tagline"><span>Your Education,</span> all in one place</p>
+            <p>Edvy is an innovative web platform designed to bridge the gap between students, teachers, and information</p>
+            <div></div>
+        </div>
+        <div className="intro-pic">
+            <img src="" alt='stacked books'/>
+        </div>
+      </section>
+  );
+}
--- edvy-app/src/index.js
+++ edvy-app/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Header from './header';
+import Home from './home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+    <Home />
   </React.StrictMode>
 );
