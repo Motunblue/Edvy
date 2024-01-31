433373c7dc2b5a4a0dcee7b66c3817da5bbe9bd9
Motunrayo <ajirotutu4real@gmail.com>
2024-01-31 18:27:40 +0100

    Update to index

--- edvy-app/src/components/offer_card.js
+++ edvy-app/src/components/offer_card.js
@@ -1,24 +1,41 @@
 import './offer_card.css';
 import React, { useState } from 'react';
 import connectIcon from './assets/connect-icon.svg';
+import collaborateIcon from './assets/collaborate-icon.svg';
+import optimizeIcon from './assets/optimize-icon.svg';
 
 export default function Offers(props) {
     const [isHovered, setHovered] = useState(false);
-    const buttonStyle = {
-        boxShadow: isHovered ? '0px 4px 4px 0px rgba(0, 0, 0, 0.25)' : 'none',
-        fontWeight: isHovered? "Bold": "inherit"
-      };
-    //const icon = props.text === "sign-up"? signupIcon: postIcon
+    const cardStyle = {
+        boxShadow: isHovered ? '0px 4px 10px 0px rgba(0, 0, 0, 0.25)' : '0px 0px 4px 0px rgba(0, 0, 0, 0.25)',
+        background: isHovered? "#FCB276": "inherit"
+    };
+    const iconStyle  = {
+        background: isHovered ? '#7BBBFB': '#DBEDFF',
+        boxShadow: isHovered ? '0px 4px 10px 0px rgba(0, 0, 0, 0.25)': null
+    };
+
+    let icon = null;
+    if (props.header === "Connect") {
+        icon = connectIcon;
+    } else if (props.header === "Collaborate") {
+        icon = collaborateIcon;
+    } else {
+        icon = optimizeIcon;
+    }
 
     return(
     <div
         onMouseEnter={() => setHovered(true)}
         onMouseLeave={() => setHovered(false)}
         className='offer-card'
+        style={cardStyle}
     >
-        <div className='offer-icon'><img src={connectIcon} alt='connect'/></div>
+        <div className='offer-icon' style={iconStyle}>
+            <img src={icon} alt='connect'/>
+        </div>
         <p>{props.header}</p>
-        <p>{props.desc}</p>
+        <p className='desc'>{props.desc}</p>
     </div>
     );
 }
---
+++ edvy-app/src/components/school.js
@@ -0,0 +1,10 @@
+import './school.css';
+import React, { useState } from 'react';
+
+export default function Schools() {
+    return (
+        <div className='schools-cont'>
+          <div className='schools-logos'></div>
+        </div>
+    );
+}
