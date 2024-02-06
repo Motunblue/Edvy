532d293af6930567f6c4a7b950722198ef8578d3
Ajibose <ajiboseibrahim12@gmail.com>
2024-02-06 23:37:08 +0000

    Update test file

--- test.py
+++ test.py
@@ -5,5 +5,12 @@ from models.school import School
 
 sc = School(name="unilag", phone_number="1362781", admin_name="qr") 
 sc.save()
-st = Student(first_name="Ibrahim", last_name="Ajibose", password="12345", school_id="UNI-0002", class_id="cls2")
+st = Student(first_name="Ibrahim", last_name="Ajibose", password="12345", school_id=sc.id)
 st.save()
+associated_school = st.school
+school_name = associated_school.name
+print(school_name)
+associated_students = sc.students
+print(sc.students)
+for student in associated_students:
+    print(student.id)
