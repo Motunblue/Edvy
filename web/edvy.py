877db052dce9911c28eb0803074457fecd2beebd
Motunrayo <ajirotutu4real@gmail.com>
2024-02-08 18:17:27 +0100

    Added registration to database

--- web/edvy.py
+++ web/edvy.py
@@ -6,9 +6,10 @@ import uuid
 from web.accounts.users import users_bp
 from web.accounts.admin import admin_bp
 from web.forms import RegistrationForm
+import bcrypt
+from models.school import School
+from web import app
 
-
-app = Flask(__name__)
 #Register blueprint for users and admin
 app.register_blueprint(users_bp)
 app.register_blueprint(admin_bp)
@@ -26,7 +27,10 @@ def signup():
     """School signup"""
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash('Account created succesfully for {}'.format(form.school_name.data), 'success')
+        hash_pwd = bcrypt.hashpw(form.password.data.encode('utf-8'), bcrypt.gensalt())
+        sc = School(name=form.school_name.data, email=form.email.data, password=hash_pwd)
+        sc.save()
+        flash('Account created succesfully for {}. Please login!'.format(form.school_name.data), 'success')
         return(redirect(url_for('admin_bp.adminLogin')))
     return render_template('signup.html', form=form, cache_id=str(uuid.uuid4()))
 
