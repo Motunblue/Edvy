7b46193517b8f44f7bc0f6e73f45590c3beedb45
Ajibose <ajiboseibrahim12@gmail.com>
2024-02-10 23:43:02 +0000

    Student registration

--- web/accounts/users.py
+++ web/accounts/users.py
@@ -2,9 +2,19 @@
 """ Users routes """
 from flask import Blueprint, render_template, redirect, url_for
 from web.forms import LoginForm
+from web.accounts.student_form import StudentRegistrationForm
 import uuid
 
 users_bp = Blueprint('user_bp', __name__, url_prefix='/') 
+
+@users_bp.route('/student/signup', methods=['GET', 'POST'], strict_slashes=False)
+def studentSignUp():
+    """Student Signup"""
+    form = StudentRegistrationForm()
+    if form.validate_on_submit():
+        flash('Account created succesfully for {}. Please login!'.format(form.student_name.data), 'success')
+        return(redirect(url_for('users_bp.studentSignUp')))
+    return render_template('users/student_signup.html', form=form, ccache_id=str(uuid.uuid4()))
 
 @users_bp.route('/login', methods=['GET', 'POST'], strict_slashes=False)
 def userLogin():
