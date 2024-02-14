af0628297104ad6fda0a65a51b8c3f5876b20019
Motunrayo <ajirotutu4real@gmail.com>
2024-02-14 10:08:17 +0100

    Added login view to login manager

--- web/accounts/admin.py
+++ web/accounts/admin.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ Admin routes """
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 from web.forms import LoginForm, PostBlog
 import uuid
 import bcrypt
 from models import storage
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 from models.post import Post
 
 admin_bp = Blueprint('admin_bp', __name__, url_prefix='/admin') 
@@ -20,7 +20,8 @@ def adminLogin():
         sc = storage.all(cls='School', email=form.email.data)
         if sc and bcrypt.checkpw(form.password.data.encode('utf-8'), sc.password.encode('utf-8')):
             login_user(sc, form.remember.data)
-            return redirect(url_for('admin_bp.adminBlog'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('admin_bp.adminBlog'))
         else:
             flash('Unsuccessful Login. Please check email or password')
     return render_template('login.html', admin=True,
@@ -28,12 +29,14 @@ def adminLogin():
                     
 
 @admin_bp.route('/blog', methods=['GET'], strict_slashes=False)
+@login_required
 def adminBlog():
     """Admin blog"""
     return render_template('users/blog.html', cached_id=str(uuid.uuid4()))
 
 
 @admin_bp.route('/blog/create-post', methods=['GET', 'POST'], strict_slashes=False)
+@login_required
 def adminPost():
     """Admin blog"""
     form = PostBlog()
